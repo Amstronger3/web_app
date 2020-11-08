1dd943991f7ab6d937b20a774d5dbab1a9db445d
Amstronger3 <61287091+Amstronger3@users.noreply.github.com>
2020-11-08 12:21:14 +0200

    savre form publication

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -8,6 +8,13 @@ class ProfileBaseForm(ModelForm):
         model = Profile
         fields = ('bio', 'location', 'birth_date', 'nickname')
         # fields = '__all__'
+        
+        
+class PublicationBaseForm(ModelForm):
+    class Meta:
+        model = Publication
+        fields = ('date', 'author', 'body', 'description', 'comment')
+        # fields = '__all__'
 
 
 class ProfileAddForm(ProfileBaseForm):

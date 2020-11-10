963efce9445cc7238d5d0e01f2a85e3fb1a0b023
Amstronger3 <61287091+Amstronger3@users.noreply.github.com>
2020-11-10 10:39:17 +0200

    add import from log.py

--- src/app/settings.py
+++ src/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from src.app.log import empty_description
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
